1ef33440cc8cb9a47b804f4c2af905a44ae07964
VergilZh <z1205067147@gmail.com>
2020-10-15 15:15:44 -0400

    Initial commit

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float horizontalBoundart;
+    public float direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void _CheckBounds()
+    {
+        //check right
+        if(transform.position.x >= horizontalBoundart)
+        {
+            direction = -1.0f;
+        }
+
+        if(transform.position.x <= -horizontalBoundart)
+        {
+            direction = 1.0f;
+        }
+    }
+
+}
